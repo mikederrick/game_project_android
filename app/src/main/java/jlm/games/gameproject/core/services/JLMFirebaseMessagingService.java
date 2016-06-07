ad976211c0061618eb22f3267d6ff94b3ff6b3e7
Mike Derrick <mike33d@gmail.com>
2016-06-07 00:30:23 -0400

    Adding push notifications
    The two services added in this commit handle registering for a
    Firebase InstanceId (which will not change throughout the app lifecycle) and a
    token, which will change throughout the lifecycle of the app (every few months or so).
    We'll need to post that to the server whenever the master branch of the server project starts
    supporting that feature.
    
    The second service (JLMFirebaseMessagingService) actually handles receiving the messages.
    Currently it just throws a notification up (whether in the background or in the foreground),
    but later on, we'll have it be a bit smarter.
    
    Both services are defined in the AndroidManifest, where we also define the intent-filters
    we want them to respond to (Firebase intent-filters in our case)

---
+++ app/src/main/java/jlm/games/gameproject/core/services/JLMFirebaseMessagingService.java
@@ -0,0 +1,48 @@
+package jlm.games.gameproject.core.services;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+import jlm.games.gameproject.HomeActivity;
+import jlm.games.gameproject.R;
+
+/**
+ * Created by sewl on 6/5/16.
+ */
+public class JLMFirebaseMessagingService extends FirebaseMessagingService {
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        String testMessage = remoteMessage.getData().get("test");
+        sendNotification("received: " + testMessage);
+    }
+
+    private void sendNotification(String messageBody) {
+        Intent intent = new Intent(this, HomeActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+                PendingIntent.FLAG_ONE_SHOT);
+
+        Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.app_icon)
+                .setContentTitle("FCM Message")
+                .setContentText(messageBody)
+                .setAutoCancel(true)
+                .setSound(defaultSoundUri)
+                .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
+    }
+}
